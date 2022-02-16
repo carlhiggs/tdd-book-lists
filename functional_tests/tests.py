7592f4474c0d9d743fd11920e255bdb27450fa97
Carl Higgs <carlhiggs@gmail.com>
2022-02-16 18:14:24 +1100

    rephrased things; this is a List of Things

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -41,15 +41,15 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url)
         
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do',self.browser.title)
+        self.assertIn('Things',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do',header_text)
+        self.assertIn('Things',header_text)
         
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
+            'Enter some Things'
         )
                
         # She types "Buy peackcock feathers" into a text box 
